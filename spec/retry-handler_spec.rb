0b59b1f442378ec44ec446c51416a5aae61b6ffb
Kensuke Nagae <kyanny@gmail.com>
2012-12-12 21:48:10 +0900

    Add spec

--- spec/retry-handler_spec.rb
+++ spec/retry-handler_spec.rb
@@ -1,7 +1,18 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "retry-handler" do
-  it "should do nothing" do
-    true.should == true
+describe RetryHandler do
+  let(:retry_count)  { 5 }
+  let(:wait) { 0.1 }
+  let(:exception) { StandardError }
+
+  describe '.retry_handler' do
+    it 'calls and retries given block' do
+      proc = Proc.new { }
+      proc.should_receive(:call).exactly(1 + retry_count).times.and_raise(exception)
+
+      expect {
+        RetryHandler.retry_handler({max: retry_count, wait: wait, accept_exception: exception}, &proc)
+      }.to raise_error(exception)
+    end
   end
 end
